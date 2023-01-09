e0e2c6f98db3014d8eafd314f97cde626d51285c
adrocodes <adriaan.jvr2018@gmail.com>
2023-01-09 11:29:51 +1100

    feat: updated library code

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # Bevy Mouse Position
+
+This plugin simple wraps the [Cookbook](https://bevy-cheatbook.github.io/cookbook/cursor2world.html) example to do the same thing. You can easily just copy and paste that example to your project if you wanted to.
+
+---
+
+## Install
+
+```
+cargo install --git https://github.com/adrocodes/bevy_mouse_position
+```
+
+## Usage
+
+```
+TODO:
+```
--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,45 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+use bevy::{prelude::*, render::camera::RenderTarget};
+
+pub struct MousePositionPlugin;
+
+#[derive(Default, Debug, Resource)]
+pub struct MousePosition(pub Vec2);
+
+impl Plugin for MousePositionPlugin {
+    fn build(&self, app: &mut App) {
+        app.insert_resource(MousePosition::default())
+            .add_system(track_mouse_position);
+    }
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+pub fn debug_mouse_position(position: Res<MousePosition>) {
+    debug!("x: {}, y: {}", position.0.x, position.0.y);
+}
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+fn track_mouse_position(
+    windows: Res<Windows>,
+    query: Query<(&Camera, &GlobalTransform)>,
+    mut position: ResMut<MousePosition>,
+) {
+    let (camera, camera_transform) = query.single();
+
+    let window = if let RenderTarget::Window(id) = camera.target {
+        windows.get(id).unwrap()
+    } else {
+        windows.get_primary().unwrap()
+    };
+
+    if let Some(screen_pos) = window.cursor_position() {
+        let window_size = Vec2::new(window.width() as f32, window.height() as f32);
+
+        let ndc = (screen_pos / window_size) * 2.0 - Vec2::ONE;
+
+        let ndc_to_world = camera_transform.compute_matrix() * camera.projection_matrix().inverse();
+
+        let world_pos = ndc_to_world.project_point3(ndc.extend(-1.0));
+
+        let world_pos: Vec2 = world_pos.truncate();
+
+        position.0 = world_pos;
     }
 }
